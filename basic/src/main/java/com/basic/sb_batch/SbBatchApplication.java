9ff2926458abbb9dc7dd3d29dd35b9e71730742c
이경용 <sleekydz86@icloud.com>
2024-01-12 17:48:45 +0900

    batch베이직
    베치파일 베이직 환경설정 셋팅

---
+++ basic/src/main/java/com/basic/sb_batch/SbBatchApplication.java
@@ -0,0 +1,13 @@
+package com.basic.sb_batch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbBatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbBatchApplication.class, args);
+	}
+
+}
