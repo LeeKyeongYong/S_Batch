09e3106d43b67c6a7d233bfa136e456a01074131
이경용 <sleekydz86@icloud.com>
2024-01-12 17:49:34 +0900

    기본버전 이벤트 테스트
    기본버전 이벤트 테스트

---
+++ basicEvent/src/test/java/com/basic/sb_batch/SbBatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.basic.sb_batch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbBatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
