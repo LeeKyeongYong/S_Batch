f45f70e87595b1922c7332212ff366d42dcea54b
이경용 <sleekydz86@icloud.com>
2024-01-13 19:05:29 +0900

    특정 타입의 빈이 하나 있을경우
    @Autowired로 로드할때 빈이름 달리 써도 된다.

---
+++ basic/src/main/java/com/basic/sb_batch/batch_job/Batch2Service.java
@@ -0,0 +1,27 @@
+package com.basic.sb_batch.batch_job;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.JobParametersBuilder;
+import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class Batch2Service {
+    private final JobLauncher jobLauncher;
+    private final Job simpleJob;
+
+    public void runSimpleJob(){
+        try{
+            JobParameters jobParameters = new JobParametersBuilder()
+                    .toJobParameters();
+            jobLauncher.run(simpleJob,jobParameters);
+
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+    }
+
+}
